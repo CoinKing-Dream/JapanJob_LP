313238cbb8a78aa1cbaf50ad628f54ea50e7dd87
MitsuiMinoru <mitsuimino711@gamil.com>
2024-08-06 05:40:02 +1000

    Update...

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import "./App.css";
-import Rating from '@mui/material/Rating';
+import Rating from "@mui/material/Rating";
 import Favorite from "@mui/icons-material/Favorite";
+import ForwardToInboxOutlinedIcon from "@mui/icons-material/ForwardToInboxOutlined";
 import CloudDownloadOutlinedIcon from "@mui/icons-material/CloudDownloadOutlined";
 
 import logo from "./assets/img/logo.svg";
+import riskmaPDF from "./assets/pdf/RisKma.pdf";
 
 function App() {
-  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   const sympathetic = {
     question: "こんなお悩みはありませんか？",
     answer: [
@@ -39,24 +40,26 @@ function App() {
       content: "B",
       img: {
         src: "",
-        alt: ""
-      }
-    }, {
+        alt: "",
+      },
+    },
+    {
       title: "A",
       content: "B",
       img: {
         src: "",
-        alt: ""
-      }
-    }, {
+        alt: "",
+      },
+    },
+    {
       title: "A",
       content: "B",
       img: {
         src: "",
-        alt: ""
-      }
-    }, 
-  ]
+        alt: "",
+      },
+    },
+  ];
 
   useEffect(() => {
     AOS.init();
@@ -66,12 +69,15 @@ function App() {
   return (
     <div className="w-full flex justify-center items-center">
       <div className="w-full flex flex-col gap-[100px]">
-        <div className="sky-background flex flex-col justify-center items-center gap-12 text-white pt-[100px] pb-[200px] relative">
+        <div className="sky-background flex flex-col justify-center items-center gap-12 text-white pt-[100px] pb-[200px] relative rounded-b-lg">
           <div className="absolute bg-white w-[60%] h-[100px] bottom-0 rounded-t-full"></div>
-          <div className="w-full text-left px-[200px] pb-[50px]">
-            <a href="">
-              <img width="200" src={logo} alt="Logo"></img>
-            </a>
+          <div className="w-full px-[200px] pb-[50px] ">
+            <div className="w-[200px] flex flex-col jusity-center items-center">
+              <a href="" className="logo-animation">
+                <img width="200" src={logo} alt="Logo"></img>
+              </a>
+              <div className="bg-gray-300 w-[100px] h-[30px] logo-shadow-animation"></div>
+            </div>
           </div>
           <div
             className="text-7xl text-center"
@@ -114,7 +120,7 @@ function App() {
           </div>
         </div>
         <div
-          className="flex justify-center items-center bg-lime-500 font-bold py-[100px] text-white"
+          className="flex justify-center items-center bg-lime-500 font-bold py-[100px] text-white rounded-[20px]"
           data-aos="fade-up"
           data-aos-delay="50"
           data-aos-easing="easing-in-out"
@@ -132,10 +138,14 @@ function App() {
           data-aos-delay="50"
           data-aos-easing="easing-in-out"
         >
-          <button className="px-[100px] py-[20px] rounded-full text-2xl font-bold text-white bg-sky-500 hover:bg-sky-700">
+          <a
+            href={riskmaPDF}
+            target="_blank"
+            className="px-[100px] py-[20px] rounded-full text-2xl font-bold text-white bg-sky-500 hover:bg-sky-700 hover:text-white"
+          >
             <span>パンフレットのダウンロードはこちら！</span>
             <CloudDownloadOutlinedIcon />
-          </button>
+          </a>
         </div>
         <div
           className="flex flex-row justify-center items-center gap-5"
@@ -149,7 +159,6 @@ function App() {
                 <div>
                   <img src={item.img} alt="" />
                 </div>
-
                 <div>{item.contents}</div>
               </div>
             );
@@ -158,16 +167,49 @@ function App() {
 
         <div>
           <div className="flex flex-row justify-center items-center">
-            {
-              feeback.map(item => {
-                reutnr (
-                  <>
-                    
-                  </>
-                )
-              })
-            }
+            {feeback.map((item) => {
+              return (
+                <>
+                  <div>{item.title}</div>
+                </>
+              );
+            })}
           </div>
+        </div>
+
+        <div className="w-full text-center bg-sky-500 text-white py-[50px] flex flex-col justify-center items-center relative hover:bg-sky-700">
+          <div className="absolute top-0 rounded-b-[40px] bg-white w-[60%] h-[30px]"></div>
+          <div>
+            <a
+              href="https://www.cti-rains.net/mailform/form_miruone.html"
+              target="_blank"
+              className="px-30px py-15px w-[100px] bg-red-500 inline"
+            >
+              <div className="flex flex-row justify-center items-center gap-3 text-white">
+                <ForwardToInboxOutlinedIcon
+                  fontSize="large"
+                  className="email-animation"
+                />
+                <div>
+                  <div>
+                    <span className="text-2xl font-bold">
+                      （株）建設技術研究所の
+                    </span>
+                    <span className="text-3xl">
+                      IoT監視観測パッケージ「みるわん」{" "}
+                    </span>
+                  </div>
+                  <div>
+                    <span className="font-bold text-3xl underline">
+                      お問い合わせ・お見積り
+                    </span>
+                    <span>はこちら </span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div className="absolute bottom-0 rounded-t-full bg-white w-[60%] h-[30px]"></div>
         </div>
       </div>
     </div>
